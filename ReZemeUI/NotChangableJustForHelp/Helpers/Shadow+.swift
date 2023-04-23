c500c92252a198c0c50db85660854956174647fb
shokhzodsunnatov <68766751+shakhzodsunnatov@users.noreply.github.com>
2023-04-23 16:51:52 +0500

    Created Reusable  ExpandingView

--- ReZemeUI/NotChangableJustForHelp/Helpers/Shadow+.swift
+++ ReZemeUI/NotChangableJustForHelp/Helpers/Shadow+.swift
@@ -13,6 +13,11 @@ extension View {
             .shadow(color: Color("shadowColor"), radius: radius, x: 4, y: 4)
     }
     
+    func glowEasy(radius: CGFloat = 25) -> some View {
+        self
+            .shadow(color: Color("shadowColor"), radius: radius, x: 4, y: 4)
+    }
+    
     func glowHard(radius: CGFloat = 10) -> some View {
         self
             .shadow(color: Color.gray.opacity(0.5), radius: radius, x: 4, y: 4)
